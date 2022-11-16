009e4c756f6f19b7320f1b92b8efe7104907ade6
Arya Majiah <71741042+aryatripm@users.noreply.github.com>
2022-11-16 09:28:16 +0700

    initial

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie Ticket',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Text('Movie Ticket'),
+    );
+  }
+}
